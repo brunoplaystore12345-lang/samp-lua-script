2f85f7f83671e78ca1db2eb21c328da95924a0ad
brunoplaystore12345-lang <brunoplaystore12345@gmail.com>
2025-08-25 14:34:28 -0400

    Atualizar o text.lua

--- text.lua
+++ text.lua
@@ -395,7 +395,6 @@ local function BOTAOO(label, tabIndex, yPosition)
 end
 
 imgui.OnInitialize(function()
-    imgui.IO.IniFilename = nil
     local config = imgui.ImFontConfig()
     config.MergeMode = true
     config.PixelSnapH = true
